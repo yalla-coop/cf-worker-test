a05e14c0ac8e55cf482fd591e60f77e73dc117a2
Ramy Shurafa <ramyshurafa@hotmail.com>
2025-02-17 12:19:02 +0300

    add redirection test code

--- replace-cdns/src/index.js
+++ replace-cdns/src/index.js
@@ -8,8 +8,45 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import redirectsMap from './redirectsMap.js';
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World! NEWW');
+		return handleRequest(request);
 	},
 };
+
+async function handleRequest(request) {
+	const url = new URL(request.url);
+	const domain = 'https://www.yallacooperative.site';
+
+	console.log(url.pathname, '<------->');
+
+	// Check if the request is for an old page and redirect to the new one
+	if (redirectsMap[url.pathname]) {
+		return Response.redirect(`${domain}${redirectsMap[url.pathname]}`, 301);
+	}
+
+	try {
+		// Fetch the response from the origin or upstream
+		const response = await fetch(request);
+
+		// Clone the response to modify its body
+		const responseBody = await response.text();
+
+		// Replace URLs in the response body
+		const modifiedBody = responseBody
+			.replace(/https:\/\/cdn\.prod\.website-files\.com\//g, 'https://www.yallacooperative.site/assets/')
+			.replace(/https:\/\/elrha\.canto\.global\/direct\//g, 'https://www.yallacooperative.site/docs/');
+
+		// Return a new response with the modified content
+		return new Response(modifiedBody, {
+			status: response.status,
+			statusText: response.statusText,
+			headers: response.headers,
+		});
+	} catch (error) {
+		console.error('Fetch error:', error);
+		return new Response('Internal Server Error', { status: 500 });
+	}
+}
---
+++ replace-cdns/src/redirectsMap.js
@@ -0,0 +1,22 @@
+export default {
+	'/funding-opportunity/innovation-challenge-adoption-challenge-facilitating-the-adoption-of-humanitarian-wash-innovations/':
+		'/funding-opportunities/innovation-challenge-adoption-challenge-facilitating-the-adoption-of-humanitarian-wash-innovations/',
+	'/funding-opportunity/innovation-challenge-data-driven-inclusion/': '/funding-opportunities/innovation-challenge-data-driven-inclusion/',
+	'/funding-opportunity/innovation-challenge-disabilities-and-older-people-in-gbv-humanitarian-programming/':
+		'/funding-opportunities/innovation-challenge-disabilities-and-older-people-in-gbv-humanitarian-programming/',
+	'/funding-opportunity/innovation-challenge-disabilities-and-older-people-in-wash-humanitarian-programming/':
+		'/funding-opportunities/innovation-challenge-disabilities-and-older-people-in-wash-humanitarian-programming/',
+	'/funding-opportunity/innovation-challenge-faecal-sludge-treatment-for-disease-outbreaks-in-humanitarian-settings/':
+		'/funding-opportunities/innovation-challenge-faecal-sludge-treatment-for-disease-outbreaks-in-humanitarian-settings/',
+	'/funding-opportunity/innovation-challenge-inclusive-preparedness': '/funding-opportunities/innovation-challenge-inclusive-preparedness',
+	'/funding-opportunity/innovation-challenge-inclusive-preparedness/':
+		'/funding-opportunities/innovation-challenge-inclusive-preparedness/',
+	'/funding-opportunity/innovation-challenge-meaningful-participation':
+		'/funding-opportunities/innovation-challenge-meaningful-participation',
+	'/funding-opportunity/innovation-challenge-meaningful-participation/':
+		'/funding-opportunities/innovation-challenge-meaningful-participation/',
+	'/funding-opportunity/innovation-challenge-responses-to-intimate-partner-violence':
+		'/funding-opportunities/innovation-challenge-responses-to-intimate-partner-violence',
+	'/funding-opportunity/innovation-challenge-responses-to-intimate-partner-violence/':
+		'/funding-opportunities/innovation-challenge-responses-to-intimate-partner-violence/',
+};
